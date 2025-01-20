018ed2e841408bcd74dcaf7374a66bbf42661a37
hassanmasood200716 <hassanmasood9876@gmail.com>
2025-01-20 23:41:55 +0500

    again

--- components/ChatForum.js
+++ components/ChatForum.js
@@ -32,10 +32,11 @@ const user = {
     const channel = client.channel('messaging', slug, {
       image: 'https://getstream.io/random_png/?name=react',
       name: slug.toUpperCase(),
+      members: [userId],
     });
 
     setChannel(channel);
-    // channel.addMembers([userId])
+     channel.addMembers([userId])
   }, [client]);
 
   if (!client) return <div>Setting up client & connection...</div>;
